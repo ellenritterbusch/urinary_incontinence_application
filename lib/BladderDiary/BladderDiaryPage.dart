6bb3e2eac346c984a8475cb3c3be2d996de29cfe
ellenritterbusch <eritte21@student.aau.dk>
2024-03-13 14:18:25 +0100

    starten på en main

---
+++ lib/BladderDiary/BladderDiaryPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BladderDiaryPage extends StatefulWidget {
+  const BladderDiaryPage({super.key});
+
+  @override
+  State<BladderDiaryPage> createState() => _BladderDiaryPageState();
+}
+
+class _BladderDiaryPageState extends State<BladderDiaryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calendar"),
+      ),
+    );
+  }
+}
