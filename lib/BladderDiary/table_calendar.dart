5ce12bd627c4a457268851a620e1c38a904e1111
ellenritterbusch <eritte21@student.aau.dk>
2024-03-13 20:16:42 +0100

    interactiveCalender

--- lib/BladderDiary/table_calendar.dart
+++ lib/BladderDiary/table_calendar.dart
@@ -10,13 +10,41 @@ class Table_calendar extends StatefulWidget {
 }
 
 class _Table_calendarState extends State<Table_calendar> {
+DateTime today = DateTime.now();
+
+void _onDaySelected(DateTime day, DateTime focusedDay){ //funktion der sætter den valgte dag til den dag der skal være i fokus
+  setState(() {
+    today = day;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: TableCalendar(
+        locale: "en_US",
+        rowHeight: 50,
+        headerStyle: HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+        ),
+        availableGestures: AvailableGestures.all,
+        calendarStyle: CalendarStyle(
+          selectedDecoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            shape: BoxShape.circle),
+           todayDecoration: BoxDecoration(
+            color: Colors.transparent,
+            //backgroundBlendMode: BlendMode.colorBurn,
+            shape: BoxShape.circle),
+            todayTextStyle: TextStyle(color: Colors.black)
+           ),
+        
         firstDay:DateTime.utc(2024,01,01),
         lastDay: DateTime.utc(2030,01,01),
-        focusedDay: DateTime.now(), 
+        focusedDay: today,
+        onDaySelected: _onDaySelected,
+        selectedDayPredicate: (day) => isSameDay(day, today),
         ),
     );
   }
