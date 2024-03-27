c7926c6bbc610b345ca5f53c5274c15c764a5308
MajaHusum <mhusum21@student.aau.dk>
2024-03-27 14:04:11 +0100

    timepicker

--- lib/BladderDiary/BladderDiaryPage/Time_picker.dart
+++ lib/BladderDiary/BladderDiaryPage/Time_picker.dart
@@ -15,7 +15,7 @@ class _Time_pickerState extends State<Time_picker> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text("${selectedTime.hour}:${selectedTime.minute}", style: TextStyle(fontSize: 50)),
+          Text("${"${selectedTime.hour}" .padLeft(2, '0')}:${"${selectedTime.minute}" .padLeft(2, '0')}", style: TextStyle(fontSize: 50)),
           ElevatedButton(
             child: const Text("Choose time", style: TextStyle(fontSize: 12)),
            onPressed: () async {
