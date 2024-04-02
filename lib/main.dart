b23faaa33876e58c8d6b7aa4b1c7f626b5d6405d
ellenritterbusch <eritte21@student.aau.dk>
2024-04-02 13:45:57 +0200

    color change

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.amber,
+          seedColor: Colors.blue,
           brightness: Brightness.light,
         ),
       textTheme: TextTheme(
