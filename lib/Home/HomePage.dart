8c2eb991097b7e06a3e420294a170f228f783a5d
ErikLC94 <128047274+ErikLC94@users.noreply.github.com>
2024-04-18 14:36:31 +0200

    First try

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -1,6 +1,13 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:urinary_incontinence_application/bluetooth/find_devices.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:location_permissions/location_permissions.dart';
+import 'dart:io' show Platform;
 
+import 'package:urinary_incontinence_application/bluetooth/find_devices.dart';
+Uuid uconUUID = Uuid.parse("C053F866-EC1B-4BF5-9E9E-CF163AD8E8E6");
+Uuid readUUID = Uuid.parse("2A3B062F-53DD-4CB1-8EC7-731A733A582F");
+Uuid writeUUID = Uuid.parse("7132DFA5-3FF8-4A11-8425-F96C43E1B4B1");
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -9,6 +16,158 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+   final flutterReactiveBle = FlutterReactiveBle(); ///Initialize the library
+  List<DiscoveredDevice> _foundBleUARTDevices = [];
+  late StreamSubscription<DiscoveredDevice> _scanStream;
+  late Stream<ConnectionStateUpdate> _currentConnectionStream;
+  late StreamSubscription<ConnectionStateUpdate> _connection;
+  late QualifiedCharacteristic _txCharacteristic;
+  late QualifiedCharacteristic _rxCharacteristic;
+  late Stream<List<int>> _receivedDataStream;
+  late TextEditingController _dataToSendText;
+  bool _scanning = false;
+  bool _connected = false;
+  String _logTexts = "";
+  List<String> _receivedData = [];
+  int _numberOfMessagesReceived = 0;
+  void initState() {
+    super.initState();
+    _dataToSendText = TextEditingController();
+  }
+
+  void refreshScreen() {
+    setState(() {});
+  }
+
+  void _sendData() async {
+      await flutterReactiveBle.writeCharacteristicWithResponse(_rxCharacteristic, value: _dataToSendText.text.codeUnits); //Write with response
+  }
+
+   void onNewReceivedData(List<int> data) {
+    _numberOfMessagesReceived += 1;
+    _receivedData.add( "$_numberOfMessagesReceived: ${String.fromCharCodes(data)}");
+    if (_receivedData.length > 5) {
+      _receivedData.removeAt(0);
+    }
+    refreshScreen();
+  }
+
+  void _disconnect() async {
+    await _connection.cancel();
+    _connected = false;
+    refreshScreen();
+  }
+
+  void _stopScan() async {
+    await _scanStream.cancel();
+    _scanning = false;
+    refreshScreen();
+  }
+  Future<void> showNoPermissionDialog() async => showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) => AlertDialog(
+          title: const Text('No location permission '),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const Text('No location permission granted.'),
+                const Text('Location permission is required for BLE to function.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Acknowledge'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+    );
+ void _startScan() async {
+    bool goForIt=false;
+    PermissionStatus permission;
+    if (Platform.isAndroid) {
+      permission = await LocationPermissions().requestPermissions();
+      if (permission == PermissionStatus.granted)
+        goForIt=true;
+    } else if (Platform.isIOS) {
+      goForIt=true;
+    }
+    if (goForIt) { //TODO replace True with permission == PermissionStatus.granted is for IOS test
+      _foundBleUARTDevices = [];
+      _scanning = true;
+      refreshScreen();
+      _scanStream =
+          flutterReactiveBle.scanForDevices(withServices: [uconUUID]).listen(( //Device discovery
+              device) {
+            if (_foundBleUARTDevices.every((element) =>
+            element.id != device.id)) {
+              _foundBleUARTDevices.add(device);
+              refreshScreen();
+            }
+          }, onError: (Object error) {
+            _logTexts =
+                "${_logTexts}ERROR while scanning:$error \n";
+            refreshScreen();
+          }
+          );
+    }
+    else {
+      await showNoPermissionDialog();
+    }
+  }
+
+void onConnectDevice(index) {
+    _currentConnectionStream = flutterReactiveBle.connectToAdvertisingDevice( //Establishing connection
+      id:_foundBleUARTDevices[index].id,
+      prescanDuration: Duration(seconds: 1),
+      withServices: [uconUUID, readUUID, writeUUID],
+    );
+    _logTexts = "";
+    refreshScreen();
+    _connection = _currentConnectionStream.listen((event) { //Maybe Observer host device BLE status
+      var id = event.deviceId.toString();
+      switch(event.connectionState) {
+        case DeviceConnectionState.connecting:
+          {
+            _logTexts = "${_logTexts}Connecting to $id\n";
+            break;
+          }
+        case DeviceConnectionState.connected:
+          {
+            _connected = true;
+            _logTexts = "${_logTexts}Connected to $id\n";
+            _numberOfMessagesReceived = 0;
+            _receivedData = [];
+            _txCharacteristic = QualifiedCharacteristic(serviceId: uconUUID, characteristicId: writeUUID, deviceId: event.deviceId);
+            _receivedDataStream = flutterReactiveBle.subscribeToCharacteristic(_txCharacteristic); //Subscribe to characteristic
+            _receivedDataStream.listen((data) {
+               onNewReceivedData(data);
+            }, onError: (dynamic error) {
+              _logTexts = "${_logTexts}Error:$error$id\n";
+            });
+            _rxCharacteristic = QualifiedCharacteristic(serviceId: uconUUID, characteristicId: writeUUID, deviceId: event.deviceId);
+            break;
+          }
+        case DeviceConnectionState.disconnecting:
+          {
+            _connected = false;
+            _logTexts = "${_logTexts}Disconnecting from $id\n";
+            break;
+          }
+        case DeviceConnectionState.disconnected:
+          {
+            _logTexts = "${_logTexts}Disconnected from $id\n";
+            break;
+          }
+      }
+      refreshScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
      return Scaffold(
