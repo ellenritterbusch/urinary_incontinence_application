cfbeb00b98eb2450ccb847c8284fa6b60a77a84c
TheVicDK <vhbr21@student.aau.dk>
2024-04-22 11:04:36 +0200

    Merge branch 'main' of https://github.com/ellenritterbusch/urinary_incontinence_application

---
+++ lib/Notifications/SetNotifications.dart
@@ -0,0 +1,212 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:urinary_incontinence_application/BladderDiary/DailyEvaluationPage/DailyEvaluationPage.dart';
+import 'package:urinary_incontinence_application/Home/HomePage.dart';
+import 'package:urinary_incontinence_application/Notifications/NotificationsPage.dart';
+
+int id = 0;
+int dailyReminderHour = 12;
+int dailyReminderMin = 44;
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+//for initialization
+final StreamController<ReceivedNotification> didReceiveLocalNotificationStream =
+    StreamController<ReceivedNotification>.broadcast();
+
+final StreamController<String?> selectNotificationStream =
+    StreamController<String?>.broadcast();
+
+//Everything a notification can maintain
+class ReceivedNotification {
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+  });
+
+  final int id;
+  final String? title;
+  final String? body;
+  final String? payload;
+
+
+
+}
+
+String? selectedNotificationPayload;
+
+/// A notification action which triggers a url launch event
+const String urlLaunchActionId = 'id_1';
+
+/// A notification action which triggers a App navigation event
+const String navigationActionId = '/DailyEvaluationPage';
+
+/// Defines a iOS/MacOS notification category for text input actions.
+const String darwinNotificationCategoryText = 'textCategory';
+
+/// Defines a iOS/MacOS notification category for plain actions.
+const String darwinNotificationCategoryPlain = 'plainCategory';
+
+@pragma('vm:entry-point')
+void notificationTapBackground(NotificationResponse notificationResponse) {
+  // ignore: avoid_print
+  print('notification(${notificationResponse.id}) action tapped: '
+      '${notificationResponse.actionId} with'
+      ' payload: ${notificationResponse.payload}');
+  if (notificationResponse.input?.isNotEmpty ?? false) {
+    // ignore: avoid_print
+    print(
+        'notification action tapped with input: ${notificationResponse.input}');
+  }
+}
+
+
+// Initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+ class SetNotifications {
+
+  static final onClickNotification = BehaviorSubject<String>();
+
+  // on tap on any notification
+  static void onNotificationTap(NotificationResponse notificationResponse) {
+    onClickNotification.add(notificationResponse.payload!);
+  }
+
+ static Future initializeNotification() async{
+
+  const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon');      //Icon wich will appear in out notification
+
+  final DarwinInitializationSettings initializationSettingsDarwin =
+    DarwinInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+        onDidReceiveLocalNotification: 
+        (int id, String? title, String? body, String? payload) async{
+          didReceiveLocalNotificationStream.add(
+        ReceivedNotification(
+          id: id,
+          title: title,
+          body: body,
+          payload: payload,
+        )); 
+      });
+        
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin,
+    macOS: initializationSettingsDarwin);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: onNotificationTap,
+  //           (NotificationResponse notificationResponse) async {
+  //     switch (notificationResponse.notificationResponseType) {
+  //       case NotificationResponseType.selectedNotification:
+  //         selectNotificationStream.add(notificationResponse.payload);
+  //         break;
+  //       case NotificationResponseType.selectedNotificationAction:
+  //         if (notificationResponse.actionId == navigationActionId) {
+  //           selectNotificationStream.add(notificationResponse.payload);
+  //         }
+  //         break;
+  //     }
+  //  },
+    onDidReceiveBackgroundNotificationResponse: notificationTapBackground);
+  
+ }
+
+       /////  functions  //////
+
+///// daily notification ////
+ Future<void> scheduleDailyNotification() async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'Remember to evaluate your day!',
+        '',
+        _nextInstanceOfChosenTime(),
+        const NotificationDetails(
+          android: AndroidNotificationDetails('daily notification channel id',
+              'daily notification channel name',
+              channelDescription: 'daily notification description'),
+        ),
+        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time);
+  }
+
+    tz.TZDateTime _nextInstanceOfChosenTime() {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate =
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, dailyReminderHour, dailyReminderMin);       //dailyReminder variable is the preffered time
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
+    }
+    return scheduledDate;
+  }
+
+    // action notification ///
+  Future<void> showNotificationWithActions() async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      'your channel id',
+      'your channel name',
+      channelDescription: 'your channel description',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      actions: <AndroidNotificationAction>[
+        AndroidNotificationAction(
+          urlLaunchActionId,
+          'Accident',
+          icon: DrawableResourceAndroidBitmap('food'),
+          contextual: true,
+        ),
+      ],
+    );
+
+    const DarwinNotificationDetails iosNotificationDetails =
+        DarwinNotificationDetails(
+      categoryIdentifier: darwinNotificationCategoryPlain,
+    );
+
+    const DarwinNotificationDetails macOSNotificationDetails =
+        DarwinNotificationDetails(
+      categoryIdentifier: darwinNotificationCategoryPlain,
+    );    
+
+    const NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+      iOS: iosNotificationDetails,
+      macOS: macOSNotificationDetails,
+    );
+    await flutterLocalNotificationsPlugin.show(
+        id++, 'Remember to register!', 'Register directly in the notification', notificationDetails,
+        payload: 'item z');
+  }
+
+    ///// simple notification///
+  Future<void> showNotification() async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails('your channel id', 'your channel name',
+            channelDescription: 'your channel description',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+    await flutterLocalNotificationsPlugin.show(
+        id++, 'plain title', 'plain body', notificationDetails,
+        payload: 'item x');
+  }
+}
