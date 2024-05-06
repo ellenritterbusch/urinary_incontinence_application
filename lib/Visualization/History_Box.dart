5beac0d218dd6e87067e1b7a5a65b3d01f63e579
ellenritterbusch <eritte21@student.aau.dk>
2024-05-06 10:53:41 +0200

    "indkommenteret" history box

--- lib/Visualization/History_Box.dart
+++ lib/Visualization/History_Box.dart
@@ -1,69 +1,69 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class History_Box extends StatefulWidget {
-//   const History_Box({super.key});
+class History_Box extends StatefulWidget {
+  const History_Box({super.key});
 
-//   @override
-//   State<History_Box> createState() => _History_BoxState();
-// }
+  @override
+  State<History_Box> createState() => _History_BoxState();
+}
 
-// class _History_BoxState extends State<History_Box> {
+class _History_BoxState extends State<History_Box> {
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Column(
-//       mainAxisAlignment: MainAxisAlignment.end,
-//       children: [
-//         SizedBox(
-//           width: double.infinity,
-//           height: 250.0,
-//           child: Card(color: Color.fromARGB(255, 207, 208, 207),
-//           child: 
-//           Row(
-//             children: [
-//               Column(
-//                 mainAxisAlignment: MainAxisAlignment.start,
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text('Accidents', style:
-//                     TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-//                   Text('5', style:
-//                     TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),
-//                     Padding(
-//                       padding: EdgeInsets.all(12.0)),
-//                   Text('On-demand stimulations', style:
-//                     TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-//                   Text('22', style:
-//                     TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),
-//                     Padding(
-//                       padding: EdgeInsets.all(12.0)),
-//                   Text('Continous stimulation', style:
-//                     TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-//                   Text('0 min', style:
-//                     TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),
-//           ],),
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: 250.0,
+          child: Card(color: Color.fromARGB(255, 207, 208, 207),
+          child: 
+          Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Accidents', style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                  Text('5', style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),
+                    Padding(
+                      padding: EdgeInsets.all(12.0)),
+                  Text('On-demand stimulations', style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                  Text('22', style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),
+                    Padding(
+                      padding: EdgeInsets.all(12.0)),
+                  Text('Continous stimulation', style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                  Text('0 min', style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),
+          ],),
 
-//                 VerticalDivider(
-//                   width: 100,
-//                   thickness: 2,
-//                   indent: 0,
-//                   endIndent: 0,
-//                   color: Color.fromARGB(255, 142, 141, 141),),
+                VerticalDivider(
+                  width: 100,
+                  thickness: 2,
+                  indent: 0,
+                  endIndent: 0,
+                  color: Color.fromARGB(255, 142, 141, 141),),
           
-//               Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 crossAxisAlignment: CrossAxisAlignment.center,
-//                 children: [
-//                   Text('Use of app', style:
-//                     TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-//                   Padding(
-//                     padding: EdgeInsets.all(20.0)),
-//                   Text('1h 3min', style:
-//                     TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),]
-//               )
-//           ], // children
-//           )),),
-//     ],
-//     );
-//   }
-// }
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Use of app', style:
+                    TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                  Padding(
+                    padding: EdgeInsets.all(20.0)),
+                  Text('1h 3min', style:
+                    TextStyle(fontSize: 20, fontWeight: FontWeight.normal),),]
+              )
+          ], // children
+          )),),
+    ],
+    );
+  }
+}
