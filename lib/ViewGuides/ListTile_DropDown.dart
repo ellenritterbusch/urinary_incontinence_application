30e79b37ef921e6f336b3e67aeff5ebe467c45d0
ErikLC94 <128047274+ErikLC94@users.noreply.github.com>
2024-05-08 13:52:26 +0200

    Created ViewGuidesPage and ListTile_DropDown

---
+++ lib/ViewGuides/ListTile_DropDown.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ListTile_DropDown extends StatefulWidget {
+  final String tile_Title;
+  final String tile_Text;
+  final Image ? image;
+  final String ? videoURL;
+
+  const ListTile_DropDown({required this.tile_Title, required this.tile_Text, this.image, this.videoURL});
+
+
+  @override
+ 
+  State<ListTile_DropDown> createState() => _ListTile_DropDownState();
+}
+
+class _ListTile_DropDownState extends State<ListTile_DropDown> {
+  bool _tileExpanded = false;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Card(child:
+            ExpansionTile(
+              title: Text(widget.tile_Title),
+              trailing: Icon(
+            _tileExpanded
+                ? Icons.keyboard_arrow_down_outlined
+                : Icons.keyboard_arrow_right_outlined),
+                
+          children: [Text(widget.tile_Text)],
+          onExpansionChanged: (bool expanded) {
+            setState(() {
+              _tileExpanded = expanded;
+            }
+             ); 
+             }
+             )
+    );
+  }
+}
---
+++ lib/ViewGuides/ViewGuidesPage.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:urinary_incontinence_application/ViewGuides/ListTile_DropDown.dart';
+class ViewGuidesPage extends StatefulWidget {
+  const ViewGuidesPage({super.key});
+
+  @override
+  State<ViewGuidesPage> createState() => _ViewGuidesPageState();
+  
+}
+
+class _ViewGuidesPageState extends State<ViewGuidesPage> {
+  bool _appExpanded = false;
+  bool _uconExpanded = false;  
+  String text = '';
+
+  @override
+  
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Guides')),
+      body: SingleChildScrollView( 
+        child: Column(
+        children: <Widget>[
+          Card.filled(
+            child: ExpansionTile(
+              title: Text("Guide to UCon"),
+              trailing: Icon(
+                  _uconExpanded
+                    ? Icons.keyboard_arrow_down_outlined
+                   : Icons.keyboard_arrow_right_outlined),
+                
+              children: [
+                ListTile_DropDown(
+                  tile_Title: 'UCon Overview', 
+                  tile_Text: 'Here is an overview of the UCon devices and the various buttons it has'),
+                ListTile_DropDown(  
+                  tile_Title: 'On/Off Button', 
+                  tile_Text: 'Press this button to turn on the UCon device'),
+                  ],
+              onExpansionChanged: (bool expanded) {
+                 setState(() {
+                      _uconExpanded = expanded;
+                   });
+                },
+              ),
+            ),
+          Card.filled(
+            child: ExpansionTile(
+              title: Text("Guide to app"),
+              trailing: Icon(
+                  _appExpanded
+                  ? Icons.keyboard_arrow_down_outlined
+                  : Icons.keyboard_arrow_right_outlined),
+                
+              children: const [
+                ListTile_DropDown(
+                  tile_Title: 'App Overview', 
+                  tile_Text: 'Here you can see an overview of the app'),
+                    ],
+              onExpansionChanged: (bool expanded) {
+                setState(() {
+                    _appExpanded = expanded;
+                  });
+               },
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+
+  }}
