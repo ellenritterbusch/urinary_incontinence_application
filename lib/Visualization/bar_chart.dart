906a696805191a8f81f9dbd5e49c2aa8b3ac6273
TheVicDK <vhbr21@student.aau.dk>
2024-05-09 13:01:30 +0200

    comments

--- lib/Visualization/bar_chart.dart
+++ lib/Visualization/bar_chart.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:get/get.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
 import 'package:urinary_incontinence_application/BladderDiary/CalendarPage/Table_calendar.dart';
@@ -18,14 +16,6 @@ class BarChart extends StatefulWidget {
   @override
   // ignore: no_logic_in_create_state
   State<BarChart> createState() => BarChartState();
-
-
- //BarChart({super.key});
-  // ignore: no_logic_in_create_state
-  // BarChartState createState() => BarChartState(
-  //   accident,
-  //   stimulations,
-  // );
 }
 
 class BarChartState extends State<BarChart> {
@@ -41,44 +31,17 @@ late dynamic stimulation = [];
 dynamic accidentValue;
 dynamic timeValue;
 dynamic stimulationValue;
-// String? datoo;
 
-
-    // Future getAccident() async {
-    //   final date = today.toString().substring(0,10);
-    //   final anAccident = await DatabaseManager.databaseManager.getAccidentBladderDiary(date);
-    //   final accidenta =  anAccident;
-    //   // debugPrint(accidenta);
-    //   // final int allaccident = accidenta['accident'];
-      
-    //   for (int i = 0; i > anAccident.length; i++){
-    //     dynamic individualAccident = anAccident[i];
-    //     accidentValue = individualAccident['accident'];
-    //   }
-    //   debugPrint('$anAccident');
-      
-      // setState(()  {
-      //   accident = allaccident;
-      //   debugPrint('Accidents: $accident');
-      // });
-      // return anAccident;
-      // debugPrint('${getAccident} accidents');
-      // final uheld = getAccident[0];
-      // return accidenta;
       
 List<ChartData> mapChartData = [];
-  // late individualAccident;
-  // late individualTime;
-  // late individualStimulation;
-Future<List<ChartData>> getChartData() async {
- 
-  final dato = today.toString().substring(0,10);
-  final anAccident = await DatabaseManager.databaseManager.getAccidentBladderDiary(dato);
-  final timer = await DatabaseManager.databaseManager.getTimeBladderDiary(dato);
-  final amountStimulation = await DatabaseManager.databaseManager.getOnDemandBladderDiary(dato);
+Future<List<ChartData>> getChartData() async {                                                          //Function to get data
+  final dato = today.toString().substring(0,10);                                                        //variable that is choosen date (as string) on table calendar
+  final anAccident = await DatabaseManager.databaseManager.getAccidentBladderDiary(dato);               //Accident data is retrieved from database as a List
+  final timer = await DatabaseManager.databaseManager.getTimeBladderDiary(dato);                        //hours data for choosen date is retrieved from database
+  final amountStimulation = await DatabaseManager.databaseManager.getOnDemandBladderDiary(dato);        //stimulation data for choosen date is retrieved from database
 
 
-  for (var i = 0; i < anAccident.length-1; i++){
+  for (var i = 0; i < anAccident.length-1; i++){                                                        //for loop to get data from 0 to amount of accidents for choosen date
 
     final individualAccident = anAccident[i];
     accidentValue = individualAccident['accident'];
@@ -90,157 +53,113 @@ Future<List<ChartData>> getChartData() async {
     final individualStimulation = amountStimulation[i];
     stimulationValue = individualStimulation['stimtype'];
 
-if (stimulationValue > 1) {
-  setState(() {
+if (stimulationValue > 1) {                                                //Stimulation data can be 1 or 2. We only want 1.
+  setState(() {                                                            //We set stimulation data = 2 to be 0.
     stimulationValue = 0;
   });
 
 }
 
-    mapChartData.add(
-          ChartData(
-            date: datoo,
-            accident: accidentValue,
-            amount_stimulation: stimulationValue,
+    mapChartData.add(                                                       //We add data
+          ChartData(                                                        //To the required variables in ChartData class we add:
+            date: datoo,                                                    //date data
+            accident: accidentValue,                                        //Accident data
+            amount_stimulation: stimulationValue,                           //stimulation data
           )
     );
 
-    // debugPrint('${amountStimulation}');
+    debugPrint('acci: ${accidentValue}');                                     //If these print, we enter the for loop.
+    debugPrint('time: ${datoo}');
+    debugPrint('Stim: ${stimulationValue}');
 
     
   }
 
-  // debugPrint('$stimulationValue');
   return mapChartData;
 }
 
 
-  //  Future <dynamic> getStimulation() async {
-  //     final amountStimulation = await DatabaseManager.databaseManager.getOnDemandBladderDiary(today.toString());
-  //     final stimulationa = amountStimulation;
-  //     setState(() {
-  //       stimulation = stimulationa[0];
-  //       debugPrint('Stimulations: $stimulation');
-  //     });
-  //     // debugPrint('$today');
-  //     // final num stimulationa = amountStimulation ;
-  //     // return amountStimulation;
-  //     // debugPrint('${amountStimulation} stimulations');
-  //     // return  amountStimulation;
-  //   }
-
-    // Future<List<ChartData>> returnvalue() async {
-    //   // dynamic stimuli = await getStimulation();
-    //   // dynamic accidenta = await getAccident();
-    //    List<ChartData> chartData = [                                 //Our data. Here we choose our data.
-    //       ChartData(today, accident, stimulation)
-    //   ];
-    //   return chartData;
-    // }
 
               
 
-@override //Brug future! eller lav en funktion som bliver kaldt herinde
+@override 
    void initState() {
     super.initState();
-    chartData = getChartData();
+    chartData = getChartData();             //Data = function() 
 
-  //   List<String> stars = [];
-  // for (int i = 0; i < 100; i++) {
-  //   stars.add('Hej');
-  //   debugPrint('$stars');
-  // }
-    //  getAccident();
-    // getStimulation();
-    // returnvalue();
 }           
-
-// Future<List<ChartData>> getChartData2() async {
-//   final List<ChartData> chartData = [
-//        ChartData(date:'2024-05-08 10:00:04Z',accident:20, amount_stimulation: 35),
-//         ChartData(date:'2024-05-08 12:30:04Z',accident:10, amount_stimulation: 20),
-//         ChartData(date:'2024-05-08 14:00:04Z',accident:20, amount_stimulation: 35),
-//         ChartData(date:'2024-05-08 18:30:04Z',accident:15, amount_stimulation: 10),
-//             ChartData(date:'2024-05-09 06:00:04Z',accident:20, amount_stimulation: 35),
-//              ChartData(date:'2024-05-09 06:30:04Z',accident:10, amount_stimulation: 20),
-//               ChartData(date:'2024-05-09 05:00:04Z',accident:20, amount_stimulation: 35),
-//                ChartData(date:'2024-05-09 05:30:04Z',accident:15, amount_stimulation: 10),
-//         ];
-//   return chartData;
-// }
-
 
 
   @override
     Widget build(BuildContext context) {
-        return  SizedBox(
+        return  SizedBox(                                                      //Wrap in sizedBox
           height: 350,
-          width: double.infinity,
+          width: double.infinity,                                             //Width to infinity of screen
           child: Scaffold(                                                     //We create a sizedbox to insert graph in
-            // height: 350,
-            // width: double.infinity,
-            body: FutureBuilder<List<ChartData>>(
-              future: chartData,
-              builder: (context, snapshot){
-                if (snapshot.hasData){
-                  return SfCartesianChart(
-              title: const ChartTitle(text: 'Use of On-Demand'),
-              legend:  const Legend(
-                alignment: ChartAlignment.center,
-                isVisible: true,
-                position: LegendPosition.bottom,
+            body: FutureBuilder<List<ChartData>>(                             //Wrap in futurebuilder so data can be inserted accourdingly
+              future: chartData,                                              //Data is chartData
+              builder: (context, snapshot){                                   //context is snapshot
+                if (snapshot.hasData){                                        //If we have data, we return our chart
+                  return SfCartesianChart(                                    //Chart
+              title: const ChartTitle(text: 'Use of On-Demand'),              //Chart Title
+              legend:  const Legend(                                          //Legends
+                alignment: ChartAlignment.center,                             //PLacement = center
+                isVisible: true,                                              //They are visible
+                position: LegendPosition.bottom,                              //Position is under graph
                 ),
               margin: const EdgeInsets.all(25),                                 //Margin aroung graph, so it fits
-              primaryXAxis: DateTimeAxis(
-                minimum: DateTime(today.year, today.month, today.day, 00, 0),      //Minimum time on graph is yesterday at current time.
-                maximum: DateTime(today.year, today.month, today.day, 24, 0),                                        //Maximum time on graph is chosen date
+              primaryXAxis: DateTimeAxis(                                       //Primaryaxis is a DateTimeAxis (x-axis)
+                minimum: DateTime(today.year, today.month, today.day, 00, 0),   //
+                maximum: DateTime(today.year, today.month, today.day, 24, 0),   //
                 dateFormat: DateFormat.Hm(),                                    //We want dateformat in forced 00:00
-                desiredIntervals: 4,                                            //Here im truly lost
                 interval: 4,                                                    //Interval of 4 hours
               ),
               enableSideBySideSeriesPlacement: true,                            //Makes the bars be beside each other, instead of on top of.
                plotAreaBorderWidth: 0,                                          //Creates at border around the graph
                 series: <CartesianSeries<ChartData, DateTime>> [                 //We create bar series
-                  
-                  ColumnSeries<ChartData, DateTime> (                            //First bar serie "Accidents"
-                      name:'Accidents',                                         //Name of bar
-                      dataSource: snapshot.data!,                                    //We get data from chartData
-                      width: 1,                                                 //Bar width
-                      xValueMapper: (ChartData chartData, _) => DateTime.parse(chartData.date),              //X value is data.x
-                      yValueMapper: (ChartData chartData, _) => chartData.accident,   //Y value    data.y
-                      color: Colors.yellow,                                     //COlor
+      
+                  ColumnSeries<ChartData, DateTime> (                                           //First bar serie "Accidents"
+                      name:'Accidents',                                                         //Name of bar
+                      dataSource: snapshot.data!,                                               //We get data from snapshot.data
+                      width: 1,                                                                 //Bar width (between 0-1)
+                      xValueMapper: (ChartData chartData, _) => DateTime.parse(chartData.date), //X value is data.x
+                      yValueMapper: (ChartData chartData, _) => chartData.accident,             //Y value is data.y
+                      color: const Color.fromARGB(255, 220, 202, 0),                          //Color of barseries
+                      borderColor: Colors.yellow,                                             //Bordercolor in hope of thicker bars
+                      borderWidth: 2,                                                           //Borderwidth = 2 pixels
                   ),
                   
-                  ColumnSeries<ChartData, DateTime>(                            //Second bar serie, "Stimulations"
-                      name: 'Stimulations',
-                      //opacity: 0.9,
-                      width: 1,
-                      dataSource: snapshot.data!,
-                      xValueMapper: (ChartData chartData, _) => DateTime.parse(chartData.date),
-                      yValueMapper: (ChartData chartData, _) => chartData.amount_stimulation,
-                      color: Colors.blue,
+                  ColumnSeries<ChartData, DateTime>(                             //Second bar serie, "Stimulations"
+                      name: 'Stimulations',                                      //title
+                      width: 1,                                                  //barwidth (must be between 0-1)
+                      borderColor: Colors.blue,                               //bordercolor
+                      borderWidth: 2,                                           //borderwidth = 2 pixels in hope of thick bars
+                      dataSource: snapshot.data!,                               //Datasource 
+                      xValueMapper: (ChartData chartData, _) => DateTime.parse(chartData.date),   //x-value
+                      yValueMapper: (ChartData chartData, _) => chartData.amount_stimulation,     //y-value
+                      color: Colors.blue,                                                       //color of bar
                   )
                 ]
             );
             }
-            else if (snapshot.hasError) {
+            else if (snapshot.hasError) {                                        //Error!
               return Text("${snapshot.error}");
             }
-            return const CircularProgressIndicator();
+            return const CircularProgressIndicator();                           //Returns loading indicator
            }
            ),
                 ),
         );
     } 
 }
-class ChartData {       
+class ChartData {                                                        //Class ChartData is our data
         final String date;                                               //This one is dateTime
         // ignore: non_constant_identifier_names
-        final num? accident;                                                    //This is num
+        final num? accident;                                             //This is num? (? = nullable) and our accidentdata variable
         // ignore: non_constant_identifier_names
-        final num? amount_stimulation;
+        final num? amount_stimulation;                                   //This is num? (? = nullable) and our stimulationdata variable
 
-    ChartData(
+    ChartData(                                                           //We require the variables in the class! 
           {required this.date, 
           required this.accident, 
           // ignore: non_constant_identifier_names
