841bbedfc5283f90e4669106280587098d3f5129
MajaHusum <mhusum21@student.aau.dk>
2024-05-13 09:49:38 +0200

    kommentarer i historybox

--- lib/Visualization/History_Box.dart
+++ lib/Visualization/History_Box.dart
@@ -1,10 +1,6 @@
-import 'dart:ffi';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:urinary_incontinence_application/Database/DatabaseManager.dart';
 import 'package:urinary_incontinence_application/BladderDiary/CalendarPage/Table_calendar.dart';
-import 'package:urinary_incontinence_application/Visualization/CreateFakeData.dart';
 
 
 class History_Box extends StatefulWidget {
@@ -16,10 +12,10 @@ class History_Box extends StatefulWidget {
 
 class _History_BoxState extends State<History_Box> {
   dynamic amountAccident;
-  int accidentcounter = 0;
   dynamic stimType;
-  int stimTypecounter = 0;
   dynamic stimTimeSetting;
+  int accidentcounter = 0;
+  int stimTypecounter = 0;
   int stimTimecounter = 0;
   late int hours;
   late int minutes;
@@ -29,83 +25,77 @@ class _History_BoxState extends State<History_Box> {
   @override
   void initState() {
     super.initState();
-    fetchBladderDiaryData();
+    fetchBladderDiaryData(); // updates data when entering the page
   }
 
   void fetchBladderDiaryData()  async{
-              String date = today.toString().substring(0 ,10);
-              debugPrint(date);
+              String date = today.toString().substring(0 ,10); // fetch data from today auto
                
-              accidentcounter = 0; // accidentcounter
-              amountAccident = await DatabaseManager.databaseManager.getBladderDiaryAccident(date); //snak med database
+              accidentcounter = 0; // Resetting accidentcounter
+              amountAccident = await DatabaseManager.databaseManager.getBladderDiaryAccident(date); // communication with database
               int amountAccidentlength = amountAccident.length;
-                //debugPrint('$accidentcounter');
-              for (int i = 0; i< amountAccidentlength; i++) {
+              
+              for (int i = 0; i< amountAccidentlength; i++) { // first value + next value +..+ in the length of amountAccident in database
                 dynamic individualAccident = amountAccident[i];
                 int accident = individualAccident['accident'];
-                //debugPrint('$accident'); 
                if (accident != 0) {
                  accidentcounter++;
                }
               } setState(() {
                 accidentcounter = accidentcounter;
               },);
-              //debugPrint('$accidentcounter');
 
               stimTypecounter = 0; // Resetting stimTypecounter
               stimType = await DatabaseManager.databaseManager.getBladderDiaryStimType(date); // Retrieving data from the database
-              //debugPrint('$stimType');
               int stimTypelength = stimType.length;
-              for (int i = 0; i < stimTypelength; i++) {
+
+              for (int i = 0; i < stimTypelength; i++) { // first value + next value +..+ in the length of stimType in database
                 dynamic individualStimType = stimType[i];
-                int stim = individualStimType['stimtype']; // Renamed to avoid conflict
-                  //debugPrint('$stimType'); 
-              if (stim == 1) {
+                int stim = individualStimType['stimtype']; 
+              if (stim == 1) { // stimType 1 = On-demand
                 stimTypecounter++;
                }
               } setState(() {
                 stimTypecounter = stimTypecounter;
               });
-              //debugPrint('$stimTypecounter');
-
 
              stimTimecounter = 0; // Resetting stimTimecounter
              stimTimeSetting = await DatabaseManager.databaseManager.getBladderDiaryStimTimeSetting(date); // Retrieving data from the database
-             //debugPrint('$stimTimeSetting');
              int stimTimeSettinglength = stimTimeSetting.length;
-             for (int i = 0; i < stimTimeSettinglength; i++) {
+
+             for (int i = 0; i < stimTimeSettinglength; i++) { // first value + next value +..+ in the length of stimTime in database
               dynamic individualStimTimeSetting = stimTimeSetting[i];
               int stimTime = individualStimTimeSetting['stimtimesetting'];
-                debugPrint('$stimTimeSetting');
+
               if (stimTime != 0) {
                 stimTimecounter += stimTime;
-              }
-              if (stimTimecounter >= 60) {
+              } if (stimTimecounter >= 60) {
                 int hours = stimTimecounter ~/ 60; // Get the number of hours
                 int minutes = stimTimecounter % 60; // Get the remaining minutes
                 debugPrint('$hours hour(s) $minutes minutes');
                   setState(() {
                   stimTimecounter = stimTimecounter;
-                   if (minutes == 0) {
+                  if (minutes == 0) {
                     totalStimTime = ('$hours hour(s)');
-                }  else {
-                totalStimTime = ('$hours hour(s) $minutes minutes');
-              } 
-              });
-              } else {
-                minutes = stimTimecounter;
-                setState(() {
-                  totalStimTime = ('$minutes minutes');
-                });
-              }
-   } 
-            }
+                  } else {
+                    totalStimTime = ('$hours hour(s) $minutes minutes');
+                    } 
+                    }); 
+                  } else {
+                      minutes = stimTimecounter;
+                    setState(() {
+                      totalStimTime = ('$minutes minutes');
+                    });
+                    }
+            } 
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return Column(
+  Widget build(BuildContext context) { // History box start with headline "History"
+    return Column( 
         mainAxisAlignment: MainAxisAlignment.end,
-        children: [    //const Data_Button(), kan indkommenteres hvis vi skal hente data fra i dag
-        SizedBox(height: 20),
+        children: [
+        const SizedBox(height: 20),
         const Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
@@ -115,7 +105,7 @@ class _History_BoxState extends State<History_Box> {
             TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 2, 80, 144))),
           ]
         ),
-          SizedBox(
+          SizedBox( // the box itself
             width: 350,
             height: 270.0,
           child: Card(
@@ -124,51 +114,55 @@ class _History_BoxState extends State<History_Box> {
             children: [
               const Padding(
                 padding: EdgeInsets.all(8.0),),
+              
               Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
+                
                 children: [
-                  Text('Accidents', style: //printer antal accidents
-                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 2, 80, 144)),),
-                  Text('$accidentcounter', style:
-                    TextStyle(fontSize: 30, fontWeight: FontWeight.normal, color: Color.fromARGB(255, 2, 80, 144)),),
-                    Padding(
+                  const Text('Accidents', style: 
+                    TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 2, 80, 144)),),
+                    Text('$accidentcounter', style:           //prints number of accidents
+                    const TextStyle( 
+                      fontSize: 30,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromARGB(255, 2, 80, 144)),),
+                    
+                    const Padding( //padding between number of accidents and number of on-demand stimulations
                       padding: EdgeInsets.all(8.0)),
-                  Text('On-demand stimulations', style: //printer antal on demand stimuleringer
-                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 2, 80, 144)),),
-                  Text('$stimTypecounter', style: //
-                    TextStyle(fontSize: 30, fontWeight: FontWeight.normal, color: Color.fromARGB(255, 2, 80, 144)),),
-                    Padding(
+                  
+                  const Text('On-demand stimulations', style: 
+                    TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 2, 80, 144)),),
+                    Text('$stimTypecounter', style:            //prints number of  on-demand stimulations
+                    const TextStyle(fontSize: 30,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromARGB(255, 2, 80, 144)),),
+                    
+                    const Padding( // padding between number of on-demand stimulations and total time of continouos stimulation
                       padding: EdgeInsets.all(8.0)),
-                  Text('Continous stimulation', style: //printer samlede tid continous stimulering
-                    TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 2, 80, 144)),),
-                  Text(totalStimTime, style: //
-                    TextStyle(fontSize: 30, fontWeight: FontWeight.normal, color: Color.fromARGB(255, 2, 80, 144)),),
-          ],
-          ),
-                //VerticalDivider( //den lodrette linje der skiller information i boxen
-                  //width: 100,
-                  //thickness: 2,
-                  //indent: 0,
-                  //endIndent: 0,
-                  //color: Color.fromARGB(255, 40, 85, 162),),
-
-             // const Column(
-              //  mainAxisAlignment: MainAxisAlignment.center,
-               // crossAxisAlignment: CrossAxisAlignment.center,
-                 // Text('Use of app', style:
-                   // TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                 // Padding(
-                   // padding: EdgeInsets.all(20.0)),
-                 // Text('1h 3min', style:
-                   // TextStyle(fontSize: 18, fontWeight: FontWeight.normal),),),],
-              //)
-            ]   
+                  
+                  const Text('Continuous stimulation', style: 
+                    TextStyle(fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 2, 80, 144)),),
+                    Text(totalStimTime, style: //prints total time of continuous stimulation
+                    const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.normal,
+                      color: Color.fromARGB(255, 2, 80, 144)),),
+                ], //children
+              ),
+            ]   // List<Widget> slutter
+            )
           )
           )
-    )
-          ]
+        ] // List<Widget> slutter
     );
-    // children 
   }
 }
